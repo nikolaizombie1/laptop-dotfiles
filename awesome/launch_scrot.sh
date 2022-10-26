7c5c521808e789dd61d1feac953bd590b6c85238
nikolaizombie1 <fabio.matos999@gmail.com>
2022-10-26 16:33:51 -0400

    Added copy to clipboard to launch_scrot

--- awesome/launch_scrot.sh
+++ awesome/launch_scrot.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-scrot -ou  "$HOME/Pictures/$(date +%m-%d-%H-%M-%S).png"
+DATE=$(date +%m-%d-%H-%M-%S)
+scrot -ou  "$HOME/Pictures/$DATE"
+xclip -sel clip -t image/png "$HOME/Pictures/$DATE"
